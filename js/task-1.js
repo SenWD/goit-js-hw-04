fb8a3039d4ed8442d35284fc8fcf07042a5ce3ae
SenWD <sengun.wd@gmail.com>
2024-12-16 08:10:51 +0300

    file addded and control

---
+++ js/task-1.js
@@ -0,0 +1,38 @@
+function isEnoughCapacity(products, containerSize) {  
+  
+    var totalPrice = 0;
+    var  myValues = [];
+   
+      myValues = Object.values(products);
+      
+     for(const price of myValues) {
+   
+          totalPrice = totalPrice + price;
+     }    
+   
+         if (containerSize === totalPrice || containerSize > totalPrice){
+          return true;
+          
+        } else { 
+          return false;
+             
+     }
+   }
+   
+   console.log(
+     isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+   ); // true
+   
+   console.log(
+     isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+   ); // false
+   
+   console.log(
+     isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+   ); // true
+   
+   console.log(
+     isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+   ); // false
+
+   
---
+++ js/task-3.js
@@ -0,0 +1,32 @@
+
+const profile = {
+ 
+    username: "Jacob",
+    playTime : 300,
+   
+    updatePlayTime(times){
+    
+        if   (this.playTime + times < 0) {
+        this.playTime = 0;
+        } else {
+        this.playTime += times;
+        }
+    },
+    changeUsername(newName) {
+      this.username = newName;
+    },
+  getInfo(){
+    console.log(`${this.username} has ${this.playTime} active hours!`);
+    },
+  };
+  
+  console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+  
+  profile.changeUsername("Marco");
+  console.log(profile.getInfo()); // "Marco has 300 active hours!"
+  
+  profile.updatePlayTime(20);
+  console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
+  
+  
